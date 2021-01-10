a716e25a1af1fb8a0266cd3a1baf6a005f07c35d
inxaneninja <inxaneninja@gmail.com>
2021-01-10 11:37:26 +0100

    Added documentation

--- src/cacher.rs
+++ src/cacher.rs
@@ -1,5 +1,24 @@
 use std::collections::HashMap;
 
+/// A Cacher is a feature that accepts a function to calculate a result, and allows the user to ask
+/// the Cacher for the result of a specific value. The Cacher will automatically store the results
+/// inside of a HashMap, so when you ask for the same value twice, the result will only be
+/// calculated once.
+///
+/// Basic usage:
+/// ```
+/// use inxane_utils::Cacher;
+///
+/// // Construct a new Cacher, with a function that simply returns it's input * 2.
+/// let mut cache = Cacher::new(|num| {
+///     num * 2
+/// });
+///
+/// println!("{}", cache.value(15)); // First calculation occurs here
+/// println!("{}", cache.value(15)); // No calculation here! It'll just take the result out of the cache.
+///
+/// assert_eq!(cache.value(15), 30);
+/// ```
 pub struct Cacher<Func, FuncArg, FuncRet> {
     func: Func,
     results: HashMap<FuncArg, FuncRet>
@@ -10,12 +29,21 @@ impl<Func, FuncArg, FuncRet> Cacher<Func, FuncArg, FuncRet>
           FuncArg: Eq + std::hash::Hash,
           FuncRet: Copy + Clone
 {
+    /// Constructs a new Cacher with the specified closure for the calculation function.
+    ///
+    /// # Arguments
+    ///
+    /// * `f` - A closure, used to calculate each result
     pub fn new(f: Func) -> Self {
         Self {
             func: f,
             results: HashMap::new()
         }
     }
+    /// Get the calculation result for the specified argument.
+    ///
+    /// Arguments:
+    /// * `v` - The value for which we want to get the calculation result.
     pub fn value(&mut self, v: FuncArg) -> FuncRet {
         match self.results.get(&v) {
             Some(result) => *result,
--- src/either.rs
+++ src/either.rs
@@ -1,3 +1,30 @@
+/// Either is a custom enum type, which holds *either* type A, or B.
+/// This can be useful for functions which want to return two different types.
+///
+/// Basic usage:
+/// ```
+/// use inxane_utils::Either;
+///
+/// // An example function that returns an i32 when n is smaller than 50,
+/// // but returns a String if it wasn't.
+/// fn test(n: i32) -> Either::<i32, String> {
+///     if n < 50 {
+///         Either::<i32, String>::Left(5)
+///     } else {
+///         Either::<i32, String>::Right(String::from("Hello!"))
+///     }
+/// }
+///
+/// // Example of checking what was returned in other code:
+///
+/// let value = test(45);
+/// // Print "It was an i32!" if the return value was an i32.
+/// // Print "It was a String!" if the return value was a String.
+/// println!("{}", match value {
+///     Either::Left(v) => "It was an i32!",
+///     Either::Right(v) => "It was a String!"
+/// });
+/// ```
 pub enum Either<A, B> {
     Left(A),
     Right(B)
